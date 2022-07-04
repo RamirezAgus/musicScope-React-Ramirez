98a938c18c3f6325b76fce813e357956b256de1a
Agustin <rlagustin0@gmail.com>
2022-07-04 14:39:47 -0300

    ItemDetail y ItemDetailContainer

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -3,15 +3,14 @@ import "./Item.css"
 
 const Item = ({products}) => {
 
-    const {name, description, price, img} = products
+    const {name, description, img} = products
 
     return (
         <div className='card align-items-center' style={{width:'10rem', margin:'1.5rem'}}>
             <img src={img} className='card-img-top' alt=''></img>
             <div className='card-body'>
-                <p className='card-text'>{name}</p>
+                <p className='card-text h5 text-center'>{name}</p>
                 <p className='card-text'>{description}</p>
-                <p className='card-text'>{price}</p>
             </div>
             <button className='btn btn-primary mb-3' id='boton-ver'>Ver más</button>
         </div>
